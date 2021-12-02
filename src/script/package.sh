8ce515d6619a59478d77b8cfa19dddf61468a918
ivyhu <ajianouc@gmail.com>
2021-12-02 17:16:30 -0500

    add packaging script

---
+++ src/script/package.sh
@@ -0,0 +1,16 @@
+DIR=$(pwd | tr "/" " " | awk '{ print $(NF) }')
+OUT=~/Coding/Algorithms/packaged/$DIR
+rm -rf $OUT
+mkdir -p $OUT
+cp *.java $OUT/
+CWD=$(pwd)
+cd $OUT
+for f in $(ls); do
+  cat $f | grep -v ^package >$f.tmp
+  mv $f.tmp $f
+done
+cd ..
+ZIPOUT=$DIR.zip
+zip -r $ZIPOUT $DIR
+rm -rf $DIR
+open .
